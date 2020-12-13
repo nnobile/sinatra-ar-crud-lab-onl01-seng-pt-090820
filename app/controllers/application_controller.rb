a5ec7aa85395619c08800785e35e3a00ca8aab6a
Nick Nobile <nickcnobile@gmail.com>
2020-12-13 13:09:10 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,29 +14,29 @@ class ApplicationController < Sinatra::Base
   # New - loads a form
   get 'articles/new' do
     erb :"articles/new"
+  end
+  
+  # Create - processes the form and creates an article
+  post '/articles' do
+    article = Article.create(params)
+    redirect "articles"
+  end
+  
+  # Index - loads all of the articles
+  get '/articles' do
+    @articles = Article.all
+    erb :"articles/index"
+  end
+  
+  # Show - gets details on a specific article
+  get '/articles/:id' do
+    @article = Article.find_by(id: params[:id])
+    if @article
+      erb :"articles/show"
+    else
+      redirect "/articles"
     end
-
-    # Create - processes the form and creates an article
-    post '/articles' do
-        article = Article.create(params)
-        redirect "articles"
-    end
- 
-    # Index - loads all of the articles
-    get '/articles' do
-        @articles = Article.all
-        erb :"articles/index"
-    end
-
-    # Show - gets details on a specific article
-    get '/articles/:id' do
-        @article = Article.find_by(id: params[:id])
-        if @article
-            erb :"articles/show"
-        else
-            redirect "/articles"
-        end
-    end
+  end
 
 
 end
