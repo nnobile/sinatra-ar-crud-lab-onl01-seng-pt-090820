79024dcd34d74b9ee579564cdb4218e59c03bc81
Nick Nobile <nickcnobile@gmail.com>
2020-12-13 12:16:50 -0800

    Done.

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,30 @@
+class ArticlesController < ApplicationController
+
+    # New - loads a form
+    get 'articles/new' do
+        erb :"articles/new"
+    end
+
+    # Create - processes the form and creates an article
+    post '/articles' do
+        article = Article.create(params)
+        redirect "articles"
+    end
+ 
+    # Index - loads all of the articles
+    get '/articles' do
+        @articles = Article.all
+        erb :"articles/index"
+    end
+
+    # Show - gets details on a specific article
+    get '/articles/:id' do
+        @article = Article.find_by(id: params[:id])
+        if @article
+            erb :"articles/show"
+        else
+            redirect "/articles"
+        end
+    end
+
+end
